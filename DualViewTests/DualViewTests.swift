df07c3a2936033b100c3ce6f5a813dc59f06f4c3
ManBand <alapchev@yandex.ru>
2024-10-18 23:42:24 +0300

    Initial Commit

---
+++ DualViewTests/DualViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  DualViewTests.swift
+//  DualViewTests
+//
+//  Created by Пупка on 18.10.2024.
+//
+
+import Testing
+@testable import DualView
+
+struct DualViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
